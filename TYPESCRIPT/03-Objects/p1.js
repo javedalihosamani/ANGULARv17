04c4daa7c9d9f111a76d45af79b1a56ca0bab122
javedalihosamani <javed.hosamani007@gmail.com>
2024-03-23 11:59:11 +0530

    Typescript Objects

---
+++ TYPESCRIPT/03-Objects/p1.js
@@ -0,0 +1,17 @@
+/* Let ObjectName = {
+    key: value => Property
+}; */
+var person = {
+    name: 'John',
+    age: 25
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+var employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+console.log(employee);
---
+++ TYPESCRIPT/03-Objects/p1.ts
@@ -0,0 +1,22 @@
+
+/* Let ObjectName = {
+    key: value => Property
+}; */
+
+let person = {
+    name: 'John',
+    age: 25
+ };
+ 
+ console.log(person);
+ console.log(person.name);
+ console.log(person.age);
+
+let employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+console.log(employee);
