4fce36163e21036d6a4a7db36b97d286fff46c11
javedalihosamani <javed.hosamani007@gmail.com>
2024-04-05 10:30:20 +0530

    Angular Router and RouterLinkActive

--- class1/src/app/app.routes.ts
+++ class1/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ServiceComponent } from './components/service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home", component: IndexComponent},
+    {path:"about", component: AboutusComponent},
+    {path:"contact", component: ContactusComponent},
+    {path:"product", component: ProductsComponent},
+    {path:"service", component: ServiceComponent},
+    {path:'', redirectTo: 'home', pathMatch:'full'}
+];
