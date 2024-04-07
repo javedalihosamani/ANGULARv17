794763f4e1ab153e640773900ba84ac893ccd435
javedalihosamani <javed.hosamani007@gmail.com>
2024-04-07 11:53:15 +0530

    *NgIf Directive example Completed

--- class5-directives/src/app/components/ng-if/ng-if.component.ts
+++ class5-directives/src/app/components/ng-if/ng-if.component.ts
@@ -1,12 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-if',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './ng-if.component.html',
   styleUrl: './ng-if.component.css'
 })
 export class NgIfComponent {
+  status: boolean = false;
 
+  StatusCheck(){
+    this.status = !this.status;
+  }
 }
