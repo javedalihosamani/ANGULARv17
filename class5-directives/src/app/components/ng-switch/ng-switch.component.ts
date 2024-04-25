5be1d846ead1afc550ab4ae36e6f0fc7fc52b148
javedalihosamani <javed.hosamani007@gmail.com>
2024-04-25 11:22:44 +0530

    Ng Switch Directive Example Completed

--- class5-directives/src/app/components/ng-switch/ng-switch.component.ts
+++ class5-directives/src/app/components/ng-switch/ng-switch.component.ts
@@ -1,12 +1,18 @@
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-switch',
   standalone: true,
-  imports: [],
+  imports: [NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './ng-switch.component.html',
   styleUrl: './ng-switch.component.css'
 })
 export class NgSwitchComponent {
+  page : string = "";
 
+  onChangeAbout(){ this.page = "about"}
+  onChangeService(){ this.page = "service"}
+  onChangeProducts(){ this.page = "product"}
+  onChangeGallery(){ this.page = "gallery"}
 }
