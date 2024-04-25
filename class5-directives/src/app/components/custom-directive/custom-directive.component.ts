70a0ef0a94b9f5e2113a685d9bc27dcbf73c3a62
javedalihosamani <javed.hosamani007@gmail.com>
2024-04-25 11:28:25 +0530

    Custom Directive functionality Completed

--- class5-directives/src/app/components/custom-directive/custom-directive.component.ts
+++ class5-directives/src/app/components/custom-directive/custom-directive.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SimpleDirective } from '../../directive/simple.directive';
+import { ScrollToTopDirective } from '../../directive/scroll-to-top.directive';
 
 @Component({
   selector: 'app-custom-directive',
   standalone: true,
-  imports: [],
+  imports: [SimpleDirective, ScrollToTopDirective],
   templateUrl: './custom-directive.component.html',
   styleUrl: './custom-directive.component.css'
 })
