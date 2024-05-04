530f972cfb666567bb72fac4a0a6edcc0af5431e
javedalihosamani <javed.hosamani007@gmail.com>
2024-05-04 12:49:33 +0530

    Router and Bootstrap Installation completed

--- class9-Animation/src/app/app.routes.ts
+++ class9-Animation/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Animation1Component } from './components/animation1/animation1.component';
+import { Animation2Component } from './components/animation2/animation2.component';
+import { Animation3Component } from './components/animation3/animation3.component';
+import { PnfComponent } from './components/pnf/pnf.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/animation1', pathMatch: 'full' },
+    { path: 'animation1', component: Animation1Component },
+    { path: 'animation2', component: Animation2Component },
+    { path: 'animation3', component: Animation3Component },
+    { path: '**', component:PnfComponent}
+];
