97e3aac82f0f79e9dab551f18bf1c55969726588
javedalihosamani <javed.hosamani007@gmail.com>
2024-05-05 11:12:33 +0530

    Animation one Example Completed

--- class9-Animation/src/app/components/animation1/animation1.component.ts
+++ class9-Animation/src/app/components/animation1/animation1.component.ts
@@ -1,3 +1,4 @@
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,8 +6,34 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './animation1.component.html',
-  styleUrl: './animation1.component.css'
+  styleUrl: './animation1.component.css',
+  animations: [
+    trigger('animateArc', [
+      state('true', style({
+        left:'400px',
+        top:'20px'
+      })),
+      state('false', style({
+        left:'0px',
+        top:'20px'
+      })),
+      transition('false=>true', animate('1000ms linear', keyframes([
+        style({ left: '0px', top: '20px', offset:0 }),
+        style({ left: '200px', top: '100px', offset:0.50 }),
+        style({ left: '400px', top: '20px',offset:1})
+      ]))),
+      transition('true=>false', animate('1000ms linear', keyframes([
+        style({ left: '400px', top: '20px', offset:0 }),
+        style({ left: '200px', top: '100px', offset:0.50 }),
+        style({ left: '0px', top: '20px',offset:1})
+      ])))
+    ])
+  ] 
 })
 export class Animation1Component {
+  arc:string = "false";
 
+  toggleBounce(){
+    this.arc = this.arc == "false"? "true" : "false";
+  }
 }
