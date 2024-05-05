8961b6553d5a662ae6d7e69bfe943a9e074c013f
javedalihosamani <javed.hosamani007@gmail.com>
2024-05-05 11:46:11 +0530

    Animation-3 example completed

--- class9-Animation/src/app/components/animation3/animation3.component.ts
+++ class9-Animation/src/app/components/animation3/animation3.component.ts
@@ -1,12 +1,40 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-animation3',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './animation3.component.html',
-  styleUrl: './animation3.component.css'
+  styleUrl: './animation3.component.css',
+  animations: [
+    trigger('slider', [
+      state('login', style({
+          backgroundColor: 'darkorange',
+          color: '#fff',
+          padding: '30px'
+      })),
+      state('signUp', style({
+          backgroundColor: 'violet',
+          color:'#fff',
+          padding:'30px'
+      })),
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('5s ease-in', style({ transform: 'translateX(0%)' }))
+      ]),
+      transition(':leave', [
+          style({ transform: 'translateX(0%)'}),
+          animate('1s ease-out', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class Animation3Component {
+  status= true;
 
+  toggleSlider() {
+    this.status = !this.status;
+  }
 }
